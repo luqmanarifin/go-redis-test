76e2061f21df802651207f34c4032ee97c9de59a
Luqman Arifin <l.arifin.siswanto@gmail.com>
2018-06-17 15:49:35 +0700

    sample code

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
-	"time"
+	"fmt"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
@@ -12,9 +13,27 @@ func main() {
 		Password: "",
 		Database: 0,
 	}
-	redis := NewRedis(opt)
-	redis.db.Set("key", "value", 10*time.Hour)
-	log.Println(redis.db.Get("key"))
+	redish := NewRedis(opt)
+	client := redish.db
+	err := client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
 
-	log.Println(redis.db.Get("asu"))
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
 }
